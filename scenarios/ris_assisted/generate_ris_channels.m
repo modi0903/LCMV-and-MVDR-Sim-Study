548433889aebdc4e5863ee82c936ce3f163f762a
modi0903 <68605782+modi0903@users.noreply.github.com>
2026-01-14 00:14:31 +0530

    Update generate_ris_channels.m
    Matrix dimensional issues were linked to this too!

--- scenarios/ris_assisted/generate_ris_channels.m
+++ scenarios/ris_assisted/generate_ris_channels.m
@@ -1,11 +1,20 @@
 function [h_eff, H_int] = generate_ris_channels(array_bs, params, Phi)
+%GENERATE_RIS_CHANNELS Effective BS-RIS-UE channel
+
+    % Convert angles to radians
     theta_bs_ris = params.theta_bs_ris * pi/180;
-    theta_ris_ue = params.theta_ris_ue * pi/180;
     theta_int = params.theta_int * pi/180;
-    h_br = steering_vector_ula(array_bs, theta_bs_ris);
+
+    % BS steering toward RIS
+    a_bs = steering_vector_ula(array_bs, theta_bs_ris);
+
+    % RIS -> UE channel (Rayleigh)
     h_ru = (randn(params.N_ris,1) + 1j*randn(params.N_ris,1)) / sqrt(2);
-    h_eff = cascaded_ris_channel(h_br, Phi, h_ru);
-    h_eff = h_eff / norm(h_eff);
+
+    % Effective desired channel (RIS-assisted)
+    h_eff = cascaded_ris_channel(a_bs, Phi, h_ru);
+
+    % Interferers (direct BS paths)
     H_int = zeros(array_bs.M, length(theta_int));
     for k = 1:length(theta_int)
         H_int(:,k) = steering_vector_ula(array_bs, theta_int(k));
